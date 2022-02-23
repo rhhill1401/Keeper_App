e1499d6f664ddf334a9de6de05f0fbee7cad69d0
Terry <rthill49@gmail.com>
2022-02-23 11:13:48 -0600

    first Commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import Note from './components/Note';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+export default App;
